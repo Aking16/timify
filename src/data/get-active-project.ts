8ae094abb976bcac0be84756f6eac6540bd6c28d
Aking <amiiri.dev@gmail.com>
2026-05-13 20:27:32 +0330

    🐛 FIX: Fixed localStorage reference error

--- src/data/get-active-project.ts
+++ src/data/get-active-project.ts
@@ -1,5 +1,7 @@
 export function getActiveProject(): { id: string | null; name: string | null } {
-  const activeProjectJSON = localStorage.getItem("active-project");
+  if (typeof window === "undefined") return { id: null, name: null };
+
+  const activeProjectJSON = window.localStorage.getItem("active-project");
   const activeProject = activeProjectJSON && JSON.parse(activeProjectJSON);
 
   if (!activeProject) return { id: null, name: null };
