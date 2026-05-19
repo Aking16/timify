d44697af1e6e15559ad670ed810b35a2e0f0a4e6
Aking <amiiri.dev@gmail.com>
2026-05-19 11:17:24 +0330

    👌 IMPROVE: Added sort for time entries

--- src/actions/time-entries/get-time-entry.ts
+++ src/actions/time-entries/get-time-entry.ts
@@ -5,7 +5,9 @@ import { timeEntries } from "@/db/schema";
 import { eq, InferSelectModel } from "drizzle-orm";
 
 export async function getTimeEntries(id: string): Promise<InferSelectModel<typeof timeEntries>[]> {
-  const data = await db.select().from(timeEntries).where(eq(timeEntries.projectId, id));
+  const data = (await db.select().from(timeEntries).where(eq(timeEntries.projectId, id))).sort(
+    (a, b) => (a.createdAt?.getTime() ?? 0) - (b.createdAt?.getTime() ?? 0)
+  );
 
   return data;
 }
